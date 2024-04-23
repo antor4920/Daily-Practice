47697289cf9316735d8ec9b43f70f8067a8c261a
antor4920 <antor4920@gmail.com>
2024-04-23 10:17:17 +0600

    3rd day

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-const arr1= ["Ashik", "jihad", "sheza", "Antor","Jeny", "Anwar", "Easmin"];
 
-let sort1= arr1.sort();
+// let changeColor= document.getElementById("hero");
+//  changeColor.style.backgroundColor="orange"
 
-console.log(sort1.toString());
+// function colorChange(){
+//     const color= ["Red", "Green", "Blue","Red", "Green", "Blue","Red", "Green", "Blue","Red", "Green", "Blue"]
+//     let colores=changeColor.style.backgroundColor=color[Math.random()*10];
+//     return colores;
+// }
+
+// color.sort(function(){return 8- Math.random()*10})
+//      return colores;
+
+const arr1= [4,59,23,75,34]
+
+console.log(arr1.sort(function(a,b){
+    return a-b;
+ }));
+
+ console.log(arr1.sort(function(a,b){
+    return b-a;
+ }))
+
+ let mathmax= Math.max.apply(null,arr1);
+ let mathmin=Math.min.apply(null,arr1);
+console.log(mathmin);
+ console.log(mathmax);
+
