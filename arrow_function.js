d88de5a28580777380838ba7738c40ae56931ad2
antor4920 <antor4920@gmail.com>
2024-04-26 22:12:52 +0600

    6th day first

---
+++ arrow_function.js
@@ -0,0 +1,21 @@
+// -------one------
+
+let f_function= (a,b) => a*b;
+
+console.log(f_function(2,6));
+
+// -----------two-------
+
+let hello= ()=>{
+    return "Hello world";
+}
+console.log(hello());
+
+// ------------three-------
+// if have only one statement and return something 
+
+let helo=() => "hello, I\'m Jahidul hasan";
+
+console.log(helo());
+
+
