82fa91720601037911f366bb115a0e4b481f9876
antor4920 <antor4920@gmail.com>
2024-10-20 18:18:52 +0600

    after a long time

--- CallBack.js
+++ CallBack.js
@@ -1,44 +1,9 @@
+const fetchData = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve("Data fetched"); // Task completed successfully
+  }, 2000);
+});
 
-function dispaly(some){
-    console.log(some);
-}
-
-function calculate(num1){
-   let result=365*num1;
-   return dispaly(result);
-
-}
-
-calculate(25);
-
-// --------Call Back Function-------
-
-function mydisplay(some){
-    console.log(some);
-}
-function calculateAge(Month,mycallback){
-     let Year= Month/12;
-     mycallback(Year);
-}
-calculateAge(20,mydisplay);
-
-
-
-
-const myNumber= [2,65,8,94,2,4,6,5,-43,-34,-6,-3];
-
-console.log(posNumber(myNumber,(x) => x>=0));
-
-function posNumber(number,callback){
-   const arr1=[];
-   for (let x of number){
-    if (callback(x)){
-        arr1.push(x);
-       
-    }
-
-   }
-   return arr1;
-   }
-  
-
+fetchData.then((data) => {
+  console.log(data); // Handle success
+});
